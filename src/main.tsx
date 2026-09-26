ab31cb992391602d58ab60558557e331e4ca6cde
koallex <koallex@mail.ru>
2026-09-26 14:18:04 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { initNativeApp } from './utils/nativeApp';
+
+// Capacitor only: status bar insets + GPS bridge. No-op in browser.
+void initNativeApp();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
